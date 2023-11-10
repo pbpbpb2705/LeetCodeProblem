7d79a9a5d3f67518bf19e26792c05c51b2e540be
Bao Pham <78841037+pbpbpb2705@users.noreply.github.com>
2023-11-10 23:55:24 +1100

    Time: 212 ms (5.37%), Space: 68.1 MB (51.81%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        vector<int> processed, processed_2;
         int tmp = nums[0], ans = 0, tmp_sum = 0, corner = nums[0];
+        bool isCorner = true;
         for (int i = 1; i < nums.size(); ++i) {
             corner = max(corner, nums[i]);
             if (nums[i] * nums[i - 1] < 0 || (nums[i - 1] == 0 && nums[i] < 0) || (nums[i] == 0 && nums[i - 1] < 0)) {
+                isCorner = false;
                 //Push back the current tmp
                 cout << tmp << " ";
-                processed.push_back(tmp);
-                if (processed.size() == 1) {
-                    if (tmp > 0) tmp_sum += tmp;
-                    tmp = nums[i];
-                    continue;
-                }
 
                 //Perform calculate
                 if (tmp > 0) {
@@ -29,18 +24,15 @@ public:
                     }
                 }
                 cout << tmp_sum << endl;
-
                 //Update tmp for next
                 tmp = nums[i];
             }
             else tmp += nums[i];
         }
-        processed.push_back(tmp);
         if (tmp > 0) tmp_sum += tmp;
-        cout << tmp << " " << tmp_sum << endl;
         ans = max(tmp_sum, ans);
-        if (processed.size() == 1) {
-            return (processed[0] >= 0) ? processed[0] : corner;
+        if (isCorner) {
+            return (nums[0] >= 0) ? ans : corner;
         }
         return ans;
     }
