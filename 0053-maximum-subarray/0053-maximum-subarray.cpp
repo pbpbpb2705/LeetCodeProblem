2d5e33f3079149dae2b71eafc73b64140328182f
Bao Pham <78841037+pbpbpb2705@users.noreply.github.com>
2023-11-10 23:54:03 +1100

    Time: 200 ms (5.37%), Space: 70.4 MB (6.06%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,29 +6,38 @@ public:
         for (int i = 1; i < nums.size(); ++i) {
             corner = max(corner, nums[i]);
             if (nums[i] * nums[i - 1] < 0 || (nums[i - 1] == 0 && nums[i] < 0) || (nums[i] == 0 && nums[i - 1] < 0)) {
+                //Push back the current tmp
+                cout << tmp << " ";
                 processed.push_back(tmp);
+                if (processed.size() == 1) {
+                    if (tmp > 0) tmp_sum += tmp;
+                    tmp = nums[i];
+                    continue;
+                }
+
+                //Perform calculate
+                if (tmp > 0) {
+                    tmp_sum += tmp;
+                }
+                else {
+                    ans = max(tmp_sum, ans);
+                    if (tmp_sum + tmp < 0) {
+                        tmp_sum = 0;
+                    }
+                    else {
+                        tmp_sum += tmp;
+                    }
+                }
+                cout << tmp_sum << endl;
+
+                //Update tmp for next
                 tmp = nums[i];
             }
             else tmp += nums[i];
         }
         processed.push_back(tmp);
-
-        for (int i = 0; i < processed.size(); ++i) {
-            if (processed[i] >= 0) tmp_sum += processed[i];
-            else {
-                if (i == processed.size() - 1) break;
-                if (processed[i] + processed[i + 1] > 0 && processed[i] + tmp_sum > 0) tmp_sum += processed[i];
-                else if (processed[i] + tmp_sum > 0) {
-                    ans = max(ans, tmp_sum);
-                    tmp_sum += processed[i];
-                }
-                else {
-                    ans = max(ans, tmp_sum);
-                    tmp_sum = 0;
-                }
-            }
-            cout << processed[i] << endl;
-        }
+        if (tmp > 0) tmp_sum += tmp;
+        cout << tmp << " " << tmp_sum << endl;
         ans = max(tmp_sum, ans);
         if (processed.size() == 1) {
             return (processed[0] >= 0) ? processed[0] : corner;
